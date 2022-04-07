75df61d811c567b24100e2f04121da351f0cfda0
TheArctesian <stephen.d.okita@gmail.com>
2022-04-07 19:00:54 +0800

    some shit was added

--- Backend/Scrapper/install.sh
+++
@@ -1,4 +0,0 @@
-# brew install go 
-go get -u github.com/golang/lint/golint
-go get -u google.golang.org/api/sheets/v4
-go get -u golang.org/x/oauth2/googlek
--- Client/flexi/android/app/src/main/kotlin/com/example/flexi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cisflexi.flexi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
