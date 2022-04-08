b97c8361cab92d2a9233bd03c118d88d5fd0bd49
theArctesian <stephen.d.okita@gmail.com>
2022-04-08 13:50:24 +0800

    speach rec

---
+++ Utils/STT/install.sh
@@ -0,0 +1,5 @@
+sudo pip install speechrecognition
+# pip install pyaudio
+# pip install pyttsx3
+# sudo apt-get install python3-pyaudio
+# pip install puaudio
---
+++ Utils/STT/speach.py
@@ -0,0 +1,15 @@
+import SpeechRecognition as sr
+
+r = sr.Recognizer()
+
+with sr.AudioFile('int.mp3') as source:
+    audio = r.record(source)
+
+
+    try: 
+
+        text=r.recognize_google(audio)
+        print("Doing things")
+        print(text)
+    except:
+        print("Fucky wucky")
