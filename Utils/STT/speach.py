78e839d12c58ad5228c7ad3bfcfbbcb7ba45f2c1
theArctesian <stephen.d.okita@gmail.com>
2022-04-08 15:07:40 +0800

    color and error msg

--- Utils/STT/speach.py
+++ Utils/STT/speach.py
@@ -1,15 +1,27 @@
 import speech_recognition as sr
+import os
+from termcolor import cprint
 
+file = open("out.txt", "w")
 r = sr.Recognizer()
 
-with sr.AudioFile('test.wav') as source:
+
+
+with sr.AudioFile('int.wav') as source:
     audio = r.record(source)
 
 
     try: 
 
         text=r.recognize_google(audio)
-        print("Doing things")
+        cprint("Doing things", 'purple')
         print(text)
+        file.write(text)
+        file.close()
+        cprint("wrote to file", 'green')
+    except sr.UnknownValueError:
+        cprint("Google Speech Recognition could not understand audio", 'red')
+    except sr.RequestError as e:
+        cprint("Could not request results from Google Speech Recognition service; {0}".format(e), 'red')
     except:
-        print("Fucky wucky")
+        cprint("Fucky wucky", 'red')
