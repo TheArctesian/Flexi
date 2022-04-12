298eef3f64849851079296aac911abcbe38c854d
theArctesian <stephen.d.okita@gmail.com>
2022-04-13 04:51:17 +0800

    updates to arch.sh

--- Scripts/arch-install.sh
+++ Scripts/arch-install.sh
@@ -1,5 +1,4 @@
 # can use doas here if you want to run it as root and not have the bloat of sudo
-sudo pacman -S nodejs npm git
-npm install -g yarn
-pacman -S pip
+sudo pacman -S nodejs npm git python-pip
+sudo npm install -g yarn
 pip install termcolor
