25cec3ba09b654cbe63dd4fdd39338a182ff9b66
theArctesian <stephen.d.okita@gmail.com>
2022-04-19 07:51:58 +0800

    mac-install.sh

--- Scripts/mac-install.sh
+++ Scripts/mac-install.sh
@@ -1,9 +1,22 @@
-# you should have brew installed alr from contribute 
-# if you dont
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install nodejs
-brew install npm
-brew install python3
-brew install pip3 
-npm i -g yarn
-sudo pip3 install termcolor
+echo "Do you have bash installed (type y for yes and n for no)"
+read input
+if [ $input = "n"]
+then 
+    sudo pip3 install termcolorbin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+    brew install nodejs
+    & brew install npm
+    & brew install python3
+    & brew install pip3 
+    & npm i -g yarn
+
+fi
+
+if [ $input = "y" ] 
+then 
+    brew install nodejs
+    & brew install npm
+    & brew install python3
+    & brew install pip3 
+    & npm i -g yarn
+    & sudo pip3 install termcolor
+fi 
