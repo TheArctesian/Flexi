8b9e82795c35a8c07c6808a3a5287a68fff2ea8b
theArctesian <stephen.d.okita@gmail.com>
2022-04-19 07:57:53 +0800

    mac-install.sh

--- Scripts/mac-install.sh
+++ Scripts/mac-install.sh
@@ -2,21 +2,21 @@ echo "Do you have bash installed (type y for yes and n for no)"
 read input
 if [ $input = "n"]
 then 
-    sudo pip3 install termcolorbin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+    sudo pip3 install termcolorbin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
     brew install nodejs
-    & brew install npm
-    & brew install python3
-    & brew install pip3 
-    & npm i -g yarn
+    brew install npm
+    brew install python3
+    brew install pip3 
+    npm i -g yarn
 
 fi
 
 if [ $input = "y" ] 
 then 
     brew install nodejs
-    & brew install npm
-    & brew install python3
-    & brew install pip3 
-    & npm i -g yarn
-    & sudo pip3 install termcolor
+    brew install npm
+    brew install python3
+    brew install pip3 
+    npm i -g yarn
+    sudo pip3 install termcolor
 fi 
