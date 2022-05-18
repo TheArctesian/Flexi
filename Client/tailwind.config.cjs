1182a918603dee6d74b585e15ebcbce9edf1ecec
theArctesian <stephen.d.okita@gmail.com>
2022-05-18 21:22:26 +0800

    tailwind

---
+++ Client/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      "White" : "#FFFFFF",
+      "Pri" : "9A0A27",
+      "Sec" : "#E6931D",
+      "L-white" : "#F0F0F0",
+      "Grey" : "#6E7272",
+      "Black" : "#242424"
+    },
+    extend: {}
+  },
+  plugins: []
+};
