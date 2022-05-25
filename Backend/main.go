a1e31531c41131df36ddd7b51f937548c4cc8bc8
theArctesian <stephen.d.okita@gmail.com>
2022-05-25 19:07:14 +0800

    little update

--- Backend/main.go
+++ Backend/main.go
@@ -8,7 +8,13 @@ func main() {
     app := fiber.New()
   
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
+        return c.JSON(&fiber.Map{"test" : {
+         "Name": "Selected well being",
+         "Teacher": "Name",
+         "Link": "zoomlink",
+         "Place": "1st floor gym",
+         "Participants": ["小明", "Alex Okita", "Jamie Hooper"]
+       },})
     })
   
     app.Listen(":6000")
