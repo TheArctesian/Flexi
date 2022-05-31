3725f4efb58b72c7d7f9f752fd8eb16cb8d96c72
theArctesian <stephen.d.okita@gmail.com>
2022-05-31 10:02:06 +0800

    GROOT

---
+++ Client/src/routes/Admin/FlexiAdmin.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let isMobile: boolean = false;
+	onMount(() => {
+		isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+	});
+</script>
+
+{#if isMobile}
+	<p>No mobile support for admin page</p>
+{:else}
+	<NavDesktop />
+	<LinksDesktop />
+	<GithubDesktop />
+{/if}
---
+++ Client/src/routes/Admin/StudentAdmin.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let isMobile: boolean = false;
+	onMount(() => {
+		isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+	});
+</script>
+
+{#if isMobile}
+	<p>No mobile support for admin pannel</p>
+{:else}{/if}
