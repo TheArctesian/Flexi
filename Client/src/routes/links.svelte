94b6b3e68451645bbf7d0a37aec38d8f2e155233
theArctesian <stephen.d.okita@gmail.com>
2022-05-31 23:02:20 +0800

    pain

--- Client/src/routes/links.svelte
+++ Client/src/routes/links.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import NavDesktop from '../Components/Desktop/NavDesktop.svelte';
-	import LinksDesktop from '../Components/Desktop/LinksDesktop.svelte';
-	import GithubDesktop from '../Components/Desktop/GithubDesktop.svelte';
+	import LoginDesktop from '../Components/Desktop/LoginDesktop.svelte';
 
-	import NavMobile from '../Components/Mobile/NavMobile.svelte';
-	import LinksMobile from '../Components/Mobile/LinksMobile.svelte';
-	import GithubMobile from '../Components/Mobile/GithubMobile.svelte';
+	import LoginMobile from '../Components/Mobile/LoginMobile.svelte';
 	import { onMount } from 'svelte';
 
 	let isMobile: boolean = false;
@@ -15,11 +11,7 @@
 </script>
 
 {#if isMobile}
-	<NavMobile />
-	<LinksMobile />
-	<GithubMobile />
+	<LoginMobile />
 {:else}
-	<NavDesktop />
-	<LinksDesktop />
-	<GithubDesktop />
+	<LoginDesktop />
 {/if}
---
+++ Client/src/routes/login.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import LoginDesktop from '../Components/Desktop/LoginDesktop.svelte';
+	import LoginMobile from '../Components/Mobile/LoginMobile.svelte';
+	import { onMount } from 'svelte';
+
+	let isMobile: boolean = false;
+	onMount(() => {
+		isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+	});
+</script>
+
+{#if isMobile}
+	<LoginMobile />
+{:else}
+	<LoginDesktop />
+{/if}
