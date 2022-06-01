2a95d7cf40fc09f2e23584975c80aed233569b39
theArctesian <stephen.d.okita@gmail.com>
2022-06-01 11:39:51 +0800

    algo

---
+++ Backend/functions.go
@@ -0,0 +1,18 @@
+package functions
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
+)
+
+func sortTeacherRooms(c *fiber.Ctx) {
+	// Get all teacher rooms
+	// Check for overlaps
+	// Sort to open rooms
+}
+
+func getFromSheet(c *fiber.Ctx) {
+	// Query Google sheet
+	// Sort Them by rows
+	// Upload thingys
+}
