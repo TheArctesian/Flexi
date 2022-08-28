7c75a64b3152b91fc61f6955c4042e2e228514f6
theArctesian <stephen.d.okita@gmail.com>
2022-08-28 15:15:53 +0800

    done

--- Client/src/Components/Desktop/RoomsDesktop.svelte
+++ Client/src/Components/Desktop/RoomsDesktop.svelte
@@ -1,25 +1,32 @@
 <script lang="ts">
 	import data from '../../lib/teachers.json';
-
-	console.log(data);
+    
+    let name = "";
+    console.log(name)
+    let empty = true;
+    function findTeacher(name){
+        console.log(name);
+    }
+        
 </script>
 
+<form
+	class="bg-black-800 flex p-2 my-5 object-contain justify-around rounded-lg "
+>
+	<input
+            class="rounded border bg-aro-900 text-white-50  flex-grow m-10 border-2 px-3 py-2"
+		name="q"
+		placeholder="Enter Teacher Name"
+	>
+
+</form>
+
+    {#if empty}
+        <img src="https://img.icons8.com/material-outlined/344/search.png">
+    {:else}
+        <h1>Home</h1>
+    {/if}
+
+
 <style>
-	:global([data-svelte-typeahead]) {
-		margin: 2rem;
-		border-radius: 1rem;
-	}
-	:global([role='listbox']) {
-	}
 </style>
-
-<Typeahead
-	label=""
-	placeholder={`🔎 Search for a teacher `}
-	{data}
-	extract={(item) => item.name}
-	disable={(item) => /Carolina/.test(item.name)}
-	on:select={({ detail }) => (events = [...events, detail])}
-	on:clear={() => (events = [...events, 'clear'])} />
-
-<pre>{JSON.stringify(events)}</pre>
