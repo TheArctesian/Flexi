e141a235c9cc9a5ddbfba41ce1faba0d6d2fdc70
theArctesian <stephen.d.okita@gmail.com>
2022-09-07 14:42:43 +0800

    some stuff

--- Client/src/lib/schedule.ts
+++ Client/src/lib/schedule.ts
@@ -1,5 +1,4 @@
 import Y13 from '$lib/Data/YearGroups/Y13.json';
-import { compute_slots } from 'svelte/internal';
 
 export type Schedule = {
 	Date: string;
@@ -18,12 +17,77 @@ export function getWeekYearGroupSchedule(year: string, dates: string[]) {
 	else {
 		alert("Years apart from Y13 are not yet supported")
 	}	
+    if (year == '12') {
+		let Schedule: Schedule[] = parseSchedule(Y12, dates);
+		return Schedule;
+	}
+	else {
+		alert("Years apart from Y12 are not yet supported")
+	}
+    if (year == '11') {
+		let Schedule: Schedule[] = parseSchedule(Y11, dates);
+		return Schedule;
+	}
+	else {
+		alert("Years apart from Y11 are not yet supported")
+	}
+    if (year == '10') {
+		let Schedule: Schedule[] = parseSchedule(Y10, dates);
+		return Schedule;
+	}
+	else {
+		alert("Years apart from Y10 are not yet supported")
+	}
+    if (year == '9') {
+		let Schedule: Schedule[] = parseSchedule(Y9, dates);
+		return Schedule;
+	}
+	else {
+		alert("Years apart from Y9 are not yet supported")
+	}
+    if (year == '8') {
+		let Schedule: Schedule[] = parseSchedule(Y8, dates);
+		return Schedule;
+	}
+	else {
+		alert("Years apart from Y8 are not yet supported")
+	}
+    if (year == '7') {
+		let Schedule: Schedule[] = parseSchedule(Y7, dates);
+		return Schedule;
+	}
+	else {
+		alert("Years apart from Y7 are not yet supported")
+	}
 }
 
 export function getDayYearGroupSchedule(year:string, date:string){
 	if (year == '13') {
 		let today: Schedule = parseScheduleDay(Y13, date)
-		console.log(today)
+		return today;
+	}
+    if (year == '12') {
+		let today: Schedule = parseScheduleDay(Y12, date)
+		return today;
+	}
+    if (year == '11') {
+		let today: Schedule = parseScheduleDay(Y11, date)
+		return today;
+	}
+    if (year == '10') {
+		let today: Schedule = parseScheduleDay(Y10, date)
+		return today;
+	}
+    if (year == '9') {
+		let today: Schedule = parseScheduleDay(Y9, date)
+		return today;
+	}
+    if (year == '8') {
+		let today: Schedule = parseScheduleDay(Y8, date)
+		return today;
+	}
+    if (year == '7') {
+		let today: Schedule = parseScheduleDay(Y7, date)
 		return today;
 	}
 }
